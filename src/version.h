bcaf28f0d4a76423676656f2ef187ef568c6d867
Rene Kuettner <rene@bitkanal.net>
2009-02-01 14:53:02 +0100

    Add version.h

---
+++ src/version.h
@@ -0,0 +1,12 @@
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define GAME_VERSION_STR    "0.0.1"
+#define GAME_VERSION_REV    1
+#define GAME_VERSION_MIN    0
+#define GAME_VERSION_MAJ    0
+#define GAME_VERSION_ID     1 
+
+#endif
+
