27de6959d5998a84b282faed8d130f82bf78fabc
Rene Kuettner <rene@bitkanal.net>
2011-02-16 12:13:32 +0100

    Add time.h inclusion.

--- client/GameRules.cpp
+++ client/GameRules.cpp
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdlib.h>
+#include <time.h>
 
 #include "Crossroad.h"
 #include "HexTile.h"
--- client/Player.cpp
+++ client/Player.cpp
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdlib.h>
+#include <time.h>
 
 #include "Game.h"
 #include "PlayerObject.h"
